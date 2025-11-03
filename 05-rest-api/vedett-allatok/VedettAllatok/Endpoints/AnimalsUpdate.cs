f6f7f50ac784f861eb91d499dcbee433fbc66100
Nagy Zétény <nagyzeteny6@gmail.com>
2025-11-03 16:42:08 +0100

    simplify update

--- 05-rest-api/vedett-allatok/VedettAllatok/Endpoints/AnimalsUpdate.cs
+++ 05-rest-api/vedett-allatok/VedettAllatok/Endpoints/AnimalsUpdate.cs
@@ -14,22 +14,15 @@ namespace VedettAllatok.Endpoints
             Description(b => b
                 .Produces(200)
                 .Produces(400)
-                .Produces(404)
-                .Produces(500));
+                .Produces(404));
         }
 
         public override async Task HandleAsync(AnimalData req, CancellationToken ct)
         {
             int id = Route<int>("id");
 
-            if (_animalStore.GetById(id) is null)
-            {
-                await Send.NotFoundAsync(ct);
-                return;
-            }
-
             bool isUpdated = _animalStore.Update(id, req);
-            if (!isUpdated) await Send.StatusCodeAsync(500, ct);
+            if (!isUpdated) await Send.NotFoundAsync(ct);
 
             await Send.StatusCodeAsync(200, ct);
         }
