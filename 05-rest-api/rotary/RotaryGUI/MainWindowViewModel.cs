ade0d30179fdb2217254b0cab7512daee7ac1576
Nagy Zétény <nagyzeteny6@gmail.com>
2025-11-10 13:29:42 +0100

    add full gui solution

--- 05-rest-api/rotary/RotaryGUI/MainWindowViewModel.cs
+++ 05-rest-api/rotary/RotaryGUI/MainWindowViewModel.cs
@@ -10,6 +10,17 @@ namespace RotaryGUI
     {
         private readonly ApiClient _api;
 
+        private string? selectedCategory;
+        private string? selectedCountryCode;
+        private string selectedGender = "Férfi";
+        private string? entryNumber;
+        private string? position;
+        private string? birthYear;
+        private string? city;
+        private string? categoryPosition;
+        private string? genderPosition;
+        private string? time;
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public RelayCommand SubmitCommand { get; }
@@ -17,16 +28,105 @@ namespace RotaryGUI
         public IEnumerable<int>? Categories { get; set; }
         public IEnumerable<string>? CountryCodes { get; set; }
 
-        public string? SelectedCategory { get; set; }
-        public string? SelectedCountryCode { get; set; }
-        public string SelectedGender { get; set; } = "Férfi";
-        public string? EntryNumber { get; set; }
-        public string? Position { get; set; }
-        public string? BirthYear { get; set; }
-        public string? City { get; set; }
-        public string? CategoryPosition { get; set; }
-        public string? GenderPosition { get; set; }
-        public string? Time { get; set; }
+        public string? SelectedCategory
+        {
+            get => selectedCategory;
+            set
+            {
+                selectedCategory = value;
+                Changed();
+            }
+        }
+
+        public string? SelectedCountryCode
+        {
+            get => selectedCountryCode;
+            set
+            {
+                selectedCountryCode = value;
+                Changed();
+            }
+        }
+
+        public string SelectedGender
+        {
+            get => selectedGender;
+            set
+            {
+                selectedGender = value;
+                Changed();
+            }
+        }
+
+        public string? EntryNumber
+        {
+            get => entryNumber;
+            set
+            {
+                entryNumber = value;
+                Changed();
+            }
+        }
+
+        public string? Position
+        {
+            get => position;
+            set
+            {
+                position = value;
+                Changed();
+            }
+        }
+
+        public string? BirthYear 
+        { 
+            get => birthYear;
+            set
+            {
+                birthYear = value;
+                Changed();
+            }
+        }
+
+        public string? City 
+        {
+            get => city;
+            set
+            {
+                city = value;
+                Changed();
+            }
+        }
+
+        public string? CategoryPosition
+        {
+            get => categoryPosition;
+            set
+            {
+                categoryPosition = value;
+                Changed();
+            }
+        }
+
+        public string? GenderPosition 
+        { 
+            get => genderPosition;
+            set
+            {
+                genderPosition = value;
+                Changed();
+            }
+        }
+
+        public string? Time 
+        {
+            get => time;
+            set
+            {
+                time = value;
+                Changed();
+            }
+        }
 
         public MainWindowViewModel()
         {
@@ -45,9 +145,104 @@ namespace RotaryGUI
             Changed(nameof(CountryCodes));
         }
 
-        private void OnSubmit(object? obj)
+        public void ResetForm()
         {
-            MessageBox.Show("submit");
+            SelectedCategory = null;
+            SelectedCountryCode = null;
+            SelectedGender = "Férfi";
+            EntryNumber = null;
+            Position = null;
+            BirthYear = null;
+            City = null;
+            CategoryPosition = null;
+            GenderPosition = null;
+            Time = null;
+        }
+
+        private async void OnSubmit(object? obj)
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(SelectedCategory)
+                || string.IsNullOrWhiteSpace(SelectedCountryCode)
+                || string.IsNullOrWhiteSpace(SelectedGender)
+                || string.IsNullOrWhiteSpace(EntryNumber)
+                || string.IsNullOrWhiteSpace(Position)
+                || string.IsNullOrWhiteSpace(BirthYear)
+                || string.IsNullOrWhiteSpace(City)
+                || string.IsNullOrWhiteSpace(CategoryPosition)
+                || string.IsNullOrWhiteSpace(GenderPosition)
+                || string.IsNullOrWhiteSpace(Time))
+            {
+                errors.Add("Kérem töltsön ki minden mezőt!");
+            }
+
+            if (!int.TryParse(EntryNumber, out _))
+            {
+                errors.Add("A rajtszám csak szám lehet!");
+            }
+
+
+            if (!int.TryParse(Position, out _))
+            {
+                errors.Add("A pozíció csak szám lehet!");
+            }
+
+            if (!int.TryParse(BirthYear, out _))
+            {
+                errors.Add("A születési év csak szám lehet!");
+            }
+
+            if (!int.TryParse(CategoryPosition, out _))
+            {
+                errors.Add("A kategóriánkénti pozíció csak szám lehet!");
+            }
+
+            if (!int.TryParse(GenderPosition, out _))
+            {
+                errors.Add("A nemenkénti pozíció csak szám lehet!");
+            }
+
+            if (!TimeOnly.TryParse(Time, out _))
+            {
+                errors.Add("A futási időt a következő formátumban adja meg (hh:mm:ss)!");
+            }
+
+            if (errors.Count > 0)
+            {
+                MessageBox.Show(string.Join("\n", errors),
+                    "Hiba",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            var result = await _api.PostAsync<IEnumerable<string>?>("/results", new
+            {
+                Position = int.Parse(Position!),
+                EntryNumber = int.Parse(EntryNumber!),
+                BirthYear = int.Parse(BirthYear!),
+                City,
+                CountryCode = SelectedCountryCode,
+                Category = SelectedCategory,
+                CategoryPosition = int.Parse(CategoryPosition!),
+                Gender = SelectedGender,
+                GenderPosition = int.Parse(GenderPosition!),
+                Time = TimeOnly.Parse(Time!),
+            });
+
+            if (result is not null)
+            {
+                MessageBox.Show(string.Join("\n", MapErrorToHungarian.MapAll(result)),
+                    "Hiba",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            ResetForm();
         }
 
         private void Changed([CallerMemberName] string propertyName = "") =>
--- 05-rest-api/rotary/RotaryLib/ApiClient.cs
+++ 05-rest-api/rotary/RotaryLib/ApiClient.cs
@@ -1,10 +1,12 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 
 namespace RotaryLib
 {
     public class ApiClient
     {
         public static readonly HttpClient HttpClient = new();
+        public const string API_URL = "http://localhost:5252/api";
 
         private readonly JsonSerializerOptions _JsonOptions = new()
         {
@@ -14,12 +16,28 @@ namespace RotaryLib
 
         public async Task<T> GetAsync<T>(string endpoint)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5252/api" + endpoint);
+            using var request = new HttpRequestMessage(HttpMethod.Get, API_URL + endpoint);
 
             var response = await HttpClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
 
             string jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(jsonResponse, _JsonOptions)!;
+        }
+
+        public async Task<T?> PostAsync<T>(string endpoint, object payload)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Post, API_URL + endpoint);
+
+            string json = JsonSerializer.Serialize(payload, _JsonOptions);
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await HttpClient.SendAsync(request);
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrWhiteSpace(jsonResponse)) return default;
+
             return JsonSerializer.Deserialize<T>(jsonResponse, _JsonOptions)!;
         }
     }
