0f5445e193ca9879f17864e7a8e2329d6b508b4d
Nagy Zétény <nagyzeteny6@gmail.com>
2025-12-07 20:22:33 +0100

    add api client, cli app 3.a solution

---
+++ 05-rest-api/gyogyfurdok/GyogyfurdokCLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using GyogyfurdokLib;
+
+var api = new ApiClient();
+
+var allBaths = await api.GetAsync<IEnumerable<Bath>>("/furdo");
+
+#region 3.a
+Console.WriteLine("3.a feladat: Összesen " +
+    allBaths.Count(x => x.City == "Budapest") +
+    " gyógyfürdő található Budapesten.");
+#endregion
---
+++ 05-rest-api/gyogyfurdok/GyogyfurdokLib/ApiClient.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace GyogyfurdokLib
+{
+    public class ApiClient
+    {
+        public static readonly HttpClient HttpClient = new();
+        public const string API_URL = "http://localhost:5014/api";
+
+        private readonly JsonSerializerOptions _JsonOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        public async Task<T> GetAsync<T>(string endpoint)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, API_URL + endpoint);
+
+            var response = await HttpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(jsonResponse, _JsonOptions)!;
+        }
+
+        public async Task<T?> PostAsync<T>(string endpoint, object payload)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Post, API_URL + endpoint);
+
+            string json = JsonSerializer.Serialize(payload, _JsonOptions);
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await HttpClient.SendAsync(request);
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrWhiteSpace(jsonResponse)) return default;
+
+            return JsonSerializer.Deserialize<T>(jsonResponse, _JsonOptions)!;
+        }
+    }
+}
