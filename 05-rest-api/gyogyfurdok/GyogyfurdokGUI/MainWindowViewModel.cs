9847079cf40848607fc0e5f989fdf45a3130c3a3
Nagy Zétény <nagyzeteny6@gmail.com>
2025-12-07 21:34:58 +0100

    add form validation

--- 05-rest-api/gyogyfurdok/GyogyfurdokGUI/MainWindowViewModel.cs
+++ 05-rest-api/gyogyfurdok/GyogyfurdokGUI/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using GyogyfurdokLib;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace GyogyfurdokGUI
 {
@@ -54,6 +55,7 @@ namespace GyogyfurdokGUI
         public MainWindowViewModel()
         {
             _api = new ApiClient();
+            SubmitCommand = new RelayCommand(OnSubmit);
 
             _ = FetchCounties();
         }
@@ -62,6 +64,70 @@ namespace GyogyfurdokGUI
         {
             Counties = await _api.GetAsync<IEnumerable<string>>("/megye");
             Changed(nameof(Counties));
+        }
+
+        public void ResetForm()
+        {
+            Id = null;
+            Name = null;
+            City = null;
+            ZipCode = null;
+            SelectedCounty = null;
+        }
+
+        private async void OnSubmit(object? obj)
+        {
+            var errors = new List<string>();
+
+
+            string?[] requiredFields = [Id, Name, City, ZipCode, SelectedCounty];
+
+            if (requiredFields.Any(string.IsNullOrWhiteSpace))
+            {
+                errors.Add("Kérem töltsön ki minden mezőt!");
+            }
+
+            if (!int.TryParse(Id, out _))
+            {
+                errors.Add($"Az azonosító csak szám lehet!");
+            }
+
+            if (string.IsNullOrWhiteSpace(Name) || !char.IsUpper(Name[0]))
+            {
+                errors.Add($"A fürdő neve csak nagybetűvel kezdődhet!");
+            }
+
+            if (string.IsNullOrWhiteSpace(Name) || Name.Length < 5)
+            {
+                errors.Add($"A fürdő nevének minimum 5 karakterből kell állnia!");
+            }
+
+            if (string.IsNullOrWhiteSpace(City) || !char.IsUpper(City[0]))
+            {
+                errors.Add($"A település neve csak nagybetűvel kezdődhet!");
+            }
+
+            if (string.IsNullOrWhiteSpace(City) || City.Length < 2)
+            {
+                errors.Add($"A település nevének minimum 2 karakterből kell állnia!");
+            }
+
+            if (!int.TryParse(ZipCode, out int val) || val < 1000 || val > 9999)
+            {
+                errors.Add($"Az irányítószám csak négyjegyű szám lehet!");
+            }
+
+            if (errors.Count > 0)
+            {
+                MessageBox.Show(string.Join("\n", errors),
+                    "Hiba",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            ResetForm();
         }
 
         private bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
