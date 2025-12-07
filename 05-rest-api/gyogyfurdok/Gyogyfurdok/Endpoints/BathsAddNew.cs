1da155fb60590de9e10619fd3d00258ee1386e02
Nagy Zétény <nagyzeteny6@gmail.com>
2025-12-07 20:14:25 +0100

    add post bath route

---
+++ 05-rest-api/gyogyfurdok/Gyogyfurdok/Endpoints/BathsAddNew.cs
@@ -0,0 +1,30 @@
+﻿using FastEndpoints;
+using GyogyfurdokLib;
+
+namespace Gyogyfurdok.Endpoints
+{
+    public class BathsAddNew(BathsStore bathsStore) : Endpoint<Bath>
+    {
+        private readonly BathsStore _bathsStore = bathsStore;
+
+        public override void Configure()
+        {
+            Post("furdo");
+            AllowAnonymous();
+            Description(b => b
+             .Produces(201)
+             .Produces<List<string>>(400));
+        }
+
+        public override async Task HandleAsync(Bath req, CancellationToken ct)
+        {
+            if (!_bathsStore.AddNew(req, out var errors))
+            {
+                await Send.ResponseAsync(errors, 400, ct);
+                return;
+            }
+
+            await Send.StatusCodeAsync(201, ct);
+        }
+    }
+}
--- 05-rest-api/gyogyfurdok/GyogyfurdokLib/BathsStore.cs
+++ 05-rest-api/gyogyfurdok/GyogyfurdokLib/BathsStore.cs
@@ -12,5 +12,25 @@
         public IEnumerable<Bath> GetAll() => _baths;
 
         public Bath? GetById(int id) => _baths.FirstOrDefault(x => x.Id == id);
+
+        public bool AddNew(Bath bath, out List<string> errors)
+        {
+            errors = [];
+
+            if (_baths.Any(x => x.Id == bath.Id))
+            {
+                errors.Add("Bath with this \"ID\" already exists");
+            }
+
+            if (_baths.Any(x => x.Name == bath.Name))
+            {
+                errors.Add("Bath with this \"Name\" already exists");
+            }
+
+            if (errors.Count > 0) return false;
+
+            _baths.Add(bath);
+            return true;
+        }
     }
 }
